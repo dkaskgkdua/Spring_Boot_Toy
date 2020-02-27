5b131b3396f2704e7cb2f20d3699e56cca720ca6
SongMinJun <dkaskgkdua@gmail.com>
2020-02-27 21:55:22 +0900

    전체 리스트, 수정, 삭제 화면 및 기능 구현

--- src/main/java/com/naver/test/service/posts/PostsService.java
+++ src/main/java/com/naver/test/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.naver.test.service.posts;
 
 import com.naver.test.domain.posts.Posts;
 import com.naver.test.domain.posts.PostsRepository;
+import com.naver.test.web.dto.PostsListResponseDto;
 import com.naver.test.web.dto.PostsResponseDto;
 import com.naver.test.web.dto.PostsSaveRequestDto;
 import com.naver.test.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -26,8 +29,20 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
     }
+    @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id = "+ id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " +id ));
+        postsRepository.delete(posts);
     }
 }
