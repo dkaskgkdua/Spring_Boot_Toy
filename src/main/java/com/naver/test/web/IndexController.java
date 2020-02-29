b4a5a6b98d0773b32adf32db9cb8ef06a0302a62
SongMinJun <dkaskgkdua@gmail.com>
2020-02-29 16:29:44 +0900

    구글 로그인 구현 중(세션오류 있음)

--- src/main/java/com/naver/test/web/IndexController.java
+++ src/main/java/com/naver/test/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.naver.test.web;
 
+import com.naver.test.config.auth.dto.SessionUser;
+import com.naver.test.domain.user.User;
 import com.naver.test.service.posts.PostsService;
 import com.naver.test.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,14 +10,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
+
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null) {
+            System.out.println("있따");
+            System.out.println("이름 : " + user.getName());
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
